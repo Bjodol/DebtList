a1db73037551b3b4eeb8ca4be6686965d7f2099c
Bjørn Dølvik <bjorndolvik@gmail.com>
2013-01-17 21:54:21 +0100

    Implemented more gui

--- src/gui/DebtPanel.java
+++ src/gui/DebtPanel.java
@@ -1,5 +1,7 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.util.List;
@@ -15,6 +17,8 @@ public class DebtPanel extends JPanel {
 	private GridBagConstraints c;
 	private List<Debt> pendingDebts;
 	private List<Debt> confirmedDebts;
+	private JPanel outgoingReqLabel;
+	private JPanel incommingReqLabel;
 	
 	public DebtPanel() {
 		setLayout(new GridBagLayout());
@@ -25,25 +29,54 @@ public class DebtPanel extends JPanel {
 		c.gridwidth = 1; 
 		c.anchor = GridBagConstraints.WEST;
 		
+		outgoingReqLabel = new JPanel();
+		incommingReqLabel = new JPanel();
+		outgoingReqLabel.setBackground(new Color(0xFF4D4D));
+		incommingReqLabel.setBackground(new Color(0x99FF66));
+		outgoingReqLabel.setPreferredSize(new Dimension(30, 30));
+		incommingReqLabel.setPreferredSize(new Dimension(30, 30));
+		
 		pendingDebts = Session.session.getUser().getPendingDebts();
 		confirmedDebts = Session.session.getUser().getConfirmedDebts();
 		if (confirmedDebts.isEmpty() && pendingDebts.isEmpty()) {
 			add(new JLabel("No debts"), c);
 		} else if (confirmedDebts.isEmpty() && !pendingDebts.isEmpty()) {
+			c.gridwidth = 1;
 			for (int i = 0; i < pendingDebts.size(); i++) {
+				if (pendingDebts.get(i).getTo().equals(Session.session.getUser())) {
+					add(incommingReqLabel, c);
+					c.gridx++;
+				} else {
+					add(outgoingReqLabel, c);
+					c.gridx++;
+				}
+				
 				add(new SingelDebtButton(pendingDebts.get(i)), c);
+				c.gridx = 0;
 				c.gridy++;
 			}
 		} else if (!confirmedDebts.isEmpty() && pendingDebts.isEmpty()) {
+			c.gridwidth = 2;
 			for (int i = 0; i < confirmedDebts.size(); i++) {
 				add(new SingelDebtButton(confirmedDebts.get(i)), c);
 				c.gridy++;
 			}
 		} else {
+			c.gridwidth = 1;
 			for (int i = 0; i < pendingDebts.size(); i++) {
+				if (pendingDebts.get(i).getTo().equals(Session.session.getUser())) {
+					add(incommingReqLabel, c);
+					c.gridx++;
+				} else {
+					add(outgoingReqLabel, c);
+					c.gridx++;
+				}
+				
 				add(new SingelDebtButton(pendingDebts.get(i)), c);
+				c.gridx = 0;
 				c.gridy++;
 			}
+			c.gridwidth = 2;
 			for (int i = 0; i < confirmedDebts.size(); i++) {
 				add(new SingelDebtButton(confirmedDebts.get(i)), c);
 				c.gridy++;
--- src/gui/SingelDebtButton.java
+++ src/gui/SingelDebtButton.java
@@ -16,39 +16,44 @@ public class SingelDebtButton extends JButton {
 	private String infoString;
 	
 	public SingelDebtButton(logic.Debt debt) {
-	super();
-	setPreferredSize(new Dimension(400, 30));
-	
-	infoString = "";
+		super();
+		infoString = "";
 		
-	switch (debt.getStatus()) {
-		case REQUESTED:
-			System.out.println("Bøh!");
-			infoString = "Request from " + debt.getRequestedBy().getUsername() + ": ";
-			setBackground(new Color(0xFFFF66));
+		switch (debt.getStatus()) {
+			case REQUESTED:
+				if (Session.session.getUser().equals(debt.getTo())) {
+					infoString = "Request sent to " + debt.getFrom().getUsername() + ": Owes you ";
+				} else if (Session.session.getUser().equals(debt.getFrom())) {
+					infoString = "Request from " + debt.getTo().getUsername() + ": You owe " ;				
+				} else {
+					MessageDialog eDialog = new MessageDialog("Debt undecided");
+				}
+				setBackground(new Color(0xFFFF66));
+				setPreferredSize(new Dimension(400, 30));
+				break;
+			case CONFIRMED:
+				
+				//Decides if it is a good or a bad debt
+				if (Session.session.getUser().equals(debt.getTo())) {
+					setBackground(new Color(0x99FF66)); //Green
+					infoString = debt.getFrom().getUsername() + ": ";
+				} else if (Session.session.getUser().equals(debt.getFrom())) {
+					setBackground(new Color(0xFF4D4D)); //Red
+					infoString = debt.getTo().getUsername() + ": ";
+				} else {
+					MessageDialog eDialog = new MessageDialog("Debt undecided");
+				}
+				setPreferredSize(new Dimension(430, 30));
 			break;
-		case CONFIRMED:
-			
-			//Decides if it is a good or a bad debt
-			if (Session.session.getUser().equals(debt.getTo())) {
-				setBackground(new Color(0x99FF66)); //Green
-				infoString = debt.getFrom().getUsername() + ": ";
-			} else if (Session.session.getUser().equals(debt.getFrom())) {
-				setBackground(new Color(0xFF4D4D)); //Red
-				infoString = debt.getTo().getUsername() + ": ";
-			} else {
-				MessageDialog eDialog = new MessageDialog("Debt undecided");
-			}
-		break;
-
-		default:
-			MessageDialog eDialog = new MessageDialog("Debt error");
-			break;
+	
+			default:
+				MessageDialog eDialog = new MessageDialog("Debt error");
+				break;
 		}
 	
-	infoString += debt.getAmount() + " " + debt.getWhat() + ", " +'"' + debt.getComment() + '"' + "."; 
-	setText(infoString);
-	setHorizontalAlignment(LEFT);
+		infoString += debt.getAmount() + " " + debt.getWhat() + ", " +'"' + debt.getComment() + '"' + "."; 
+		setText(infoString);
+		setHorizontalAlignment(LEFT);
 	}
 
 }
